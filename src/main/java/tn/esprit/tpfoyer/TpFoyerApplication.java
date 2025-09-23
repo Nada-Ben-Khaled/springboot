0cc405a623852e0554fba8caf4ed0f5754a28d11
nadaaaaaaaa777777777 <benkhalednada18@gmail.com>
2025-09-23 20:17:33 +0100

    Ajout  les classes

--- src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
+++ src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -2,11 +2,12 @@ package tn.esprit.tpfoyer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("tn.esprit.tpfoyer.entity") // ✅ Obligatoire pour scanner les entités
 public class TpFoyerApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(TpFoyerApplication.class, args); // Correct
+        SpringApplication.run(TpFoyerApplication.class, args);
     }
 }
--- src/test/java/tn/esprit/tpfoyer/TpFoyerApplicationTests.java
+++ src/test/java/tn/esprit/tpfoyer/TpFoyerApplicationTests.java
@@ -2,8 +2,11 @@ package tn.esprit.tpfoyer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootTest
+@EntityScan("tn.esprit.tpfoyer.entity")
 class TpFoyerApplicationTests {
 
     @Test
